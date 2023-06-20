ce80b79d2c17a62146bd46f593b1304c34027fea
ivan <ippeng54@gmail.com>
2023-06-20 21:19:20 +0400

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        with open('text.txt', 'a') as f:
+            f.write(f'{name}, {phone}, {message}; ')
+        print(f'{name}, {phone}, {message}')
+    return render(request, 'catalog/contacts.html')
